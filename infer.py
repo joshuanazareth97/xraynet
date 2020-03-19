91deb1dc89d49e3626e4c118f55d521317009706
joshua <joshuanazareth97@gmail.com>
2020-03-19 12:26:42 +0000

    Final models

--- infer.py
+++ infer.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import copy
+import random
 import pandas as pd
 import torch
 from torch.autograd import Variable
@@ -8,6 +9,12 @@ from densenet import densenet169
 from torchvision import transforms
 from torchvision.datasets.folder import pil_loader
 from torch.autograd import Variable
+
+def get_tensor_from_image(path, transform=None):
+    image = pil_loader(img_path)
+    if transform: image = transform(image)
+    image = image.float()
+    return Variable(image, requires_grad=True).unsqueeze(0).cuda()
 
 assert len(sys.argv) > 1
 
@@ -17,6 +24,7 @@ model = densenet169(pretrained=True)
 model = model.cuda()
 
 model.load_state_dict(torch.load("./models/model_XR_WRIST.pth"))
+model.eval()
 
 transformer = transforms.Compose([
             transforms.Resize((224, 224)),
@@ -24,12 +32,13 @@ transformer = transforms.Compose([
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
 
-image = pil_loader(img_path)
+input_image = get_tensor_from_image(img_path, transformer)
 
-final_img = transformer(image).float()
-# import pdb; pdb.set_trace()
-final_img = Variable(final_img, requires_grad=True)
+skip = sorted(random.sample(range(36807),36807-100))
+img_paths = pd.read_csv("~/mura-data/MURA-v1.1/train_image_paths.csv", skiprows=skip, header=None)
+img_paths = "~/mura-data/" + img_paths
+import pdb; pdb.set_trace()
 
-output = model(final_img.unsqueeze(0).cuda())
+output = model(input_image)
 
-print(output)
+print(img_path, output.item())
